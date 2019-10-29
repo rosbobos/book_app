b0d16c7e100e121273ead491842b1693f85fd615
DwayneWayneJr <dwaynejgrant@gmail.com>
2019-10-29 11:52:19 -0700

    creating function for search for books

--- server.js
+++ server.js
@@ -19,6 +19,12 @@ function newSearch(request, response) {
   response.render('pages/index');
 }
 
+function searchForBooks(request, response) {
+  console.log(request.body.search);
+  const bookSearchedFor = request.body.search[0];
+  const typeOfSearch = request.body.search[1];
 
+  let url = 
+}
 
 app.listen(PORT, () => {console.log(`turned up on ${PORT}`)});
