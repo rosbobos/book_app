145c194ee008c75c08af05faa0229a7e818d0312
DwayneWayneJr <dwaynejgrant@gmail.com>
2019-10-29 11:46:12 -0700

    create function new search

--- server.js
+++ server.js
@@ -4,19 +4,21 @@ const express = require('express');
 require ('dotenv').config();
 require('ejs');
 const superagent = require('superagent');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({extended:true}));
-app.use(express.static('public'));
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
-app.get('/', function(request, response){
+app.get('/', newSearch);
+app.post('/searches', searchForBooks);
+
+function newSearch(request, response) {
   response.render('pages/index');
-});
+}
 
-app.get('/hello', function(request, response){
-  response.render('pages/error');
-});
+
 
 app.listen(PORT, () => {console.log(`turned up on ${PORT}`)});
