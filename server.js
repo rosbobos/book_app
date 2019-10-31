79542c73dee1f4b1412c69fec07e46d6b460d13f
Rosalyn Johnson <rosraejohnson@gmail.com>
2019-10-31 11:57:20 -0700

    got books form page to render as well as the search results

--- server.js
+++ server.js
@@ -23,18 +23,23 @@ client.connect()
     })
   })
 
-app.get('/', newSearch);
+app.get('/', homePage);
 // TODO: check to get all app.get for functions for new pages
 // app.get('/searches/show', )
+app.post('/searches/new', addBook);
 app.post('/searches', searchForBooks);
+app.get('/searches', searchPage);
+app.get('/searches/form', formPage);
 
 
-function newSearch(request, response) {
+function homePage(request, response) {
   const sql = 'SELECT * FROM books;';
+  // console.log('in newsearch for books');
 
   client.query(sql)
     .then(sqlResults => {
       const bookArray = sqlResults.rows;
+      console.log(bookArray);
       response.render('pages/index', {books: bookArray});      
     })
     .catch(err =>{
@@ -42,8 +47,24 @@ function newSearch(request, response) {
     })
 }
 
+function searchPage(request, response){
+  response.render('pages/searches/new');
+}
+
+function addBook(request, response){
+let {book_image, title, author, description, isb, bookshelf} = request.body;
+let sql = 'INSERT INTO book_app (book_image, title, author, description, isb, bookshelf) VALUES ($1, $2, $3, $4, $5, $6);';
+let values = [book_image, title, author, description, isb, bookshelf];
+return client.query(sql, values)
+  .then(response.redirect('/'))
+  .catch(err => console.error(err));
+}
+
+function formPage(request, response){
+  response.render('pages/searches/form');
+}
+
 function searchForBooks(request, response) {
-  console.log(request.body.search);
 
   const bookSearchedFor = request.body.search[0];
   const typeOfSearch = request.body.search[1];
@@ -77,7 +98,8 @@ function searchForBooks(request, response) {
 
 
 function Book(bookObj) {
-  const bookImage = `https://i.imgur.com/J5LVHEL.jpg`;
+  // const bookImage = `https://i.imgur.com/J5LVHEL.jpg`;
+  this.book_image = bookObj.imageLinks.thumbnail || `https://i.imgur.com/J5LVHEL.jpg`;
   this.title = bookObj.title || 'no title available';
   this.author = bookObj.author || 'no author available';
   this.description = bookObj.description || 'no description available';
